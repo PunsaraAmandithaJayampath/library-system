45a1e45170e65f0e480216637ad852afde05189e
PunsaraAmandithaJayampath <punsarase@gmail.com>
2024-02-12 10:38:51 +0530

    create database & yml file

--- src/main/java/org/example/Service/BookServiceImpl.java
+++ src/main/java/org/example/Service/BookServiceImpl.java
@@ -1,7 +1,9 @@
 package org.example.Service;
 
 import org.example.Dto.Book;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookServiceImpl implements BookService{
     @Override
     public void addBook(Book book) {
